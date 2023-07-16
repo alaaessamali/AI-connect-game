2477ccc4d939b945425990ed66c7644ad4281cfe
alaaessamali <119875178+alaaessamali@users.noreply.github.com>
2023-07-16 17:38:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,492 @@
+from threading import Timer
+
+import pygame
+import sys
+import numpy as np
+import random
+import tkinter as tk
+from pygame import math
+
+# ALPHA BITA ALGORITHMS
+# MINIMAX ALGOTITHMS
+
+# Define game constants
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+SQUARE_SIZE = 100
+RADIUS = int(SQUARE_SIZE / 2 - 5)
+width = COLUMN_COUNT * SQUARE_SIZE
+height = (ROW_COUNT + 1) * SQUARE_SIZE
+size = (width, height)
+screen = pygame.display.set_mode(size)
+
+
+# Create the game board
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+
+# Add a piece to the board
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+# Check if a move is valid
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+
+# Get the next open row in the column
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+# Print the board to the console
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+# Check if a player has won
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                c + 3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                c] == piece:
+                return True
+
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
+                c + 3] == piece:
+                return True
+
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
+                c + 3] == piece:
+                return True
+
+
+# Evaluate the score of a move
+def evaluate_window(window, piece):
+    score = 0
+    opp_piece = 1
+    if piece == 1:
+        opp_piece = 2
+
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score += 2
+
+    if window.count(opp_piece) == 3 and window.count(0) == 1:
+        score -= 4
+
+    return score
+
+
+# Evaluate the board score
+def score_position(board, piece):
+    score = 0
+
+    # Score center column
+    center_array = [int(i) for i in list(board[:, COLUMN_COUNT // 2])]
+    center_count = center_array.count(piece)
+    score += center_count * 3
+
+    # Score horizontal
+    for r in range(ROW_COUNT):
+        row_array = [int(i) for i in list(board[r, :])]
+        for c in range(COLUMN_COUNT - 3):
+            window = row_array[c:c + 4]
+            score += evaluate_window(window, piece)
+
+    # Score vertical
+    for c in range(COLUMN_COUNT):
+        col_array = [int(i) for i in list(board[:, c])]
+        for r in range(ROW_COUNT - 3):
+            window = col_array[r:r + 4]
+            score += evaluate_window(window, piece)
+
+    # Score positively sloped diagonal
+    for r in range(ROW_COUNT - 3):
+        for c in range(COLUMN_COUNT - 3):
+            window = [board[r + i][c + i] for i in range(4)]
+            score += evaluate_window(window, piece)
+
+    # Score negatively sloped diagonal
+    for r in range(ROW_COUNT - 3):
+        for c in range(COLUMN_COUNT - 3):
+            window = [board[r + 3 - i][c + i] for i in range(4)]
+            score += evaluate_window(window, piece)
+
+    return score
+
+
+# implementation of the Minimax algorithm
+def minimax(board, depth, maximizingPlayer):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, 2):
+                return (None, 100000000000000)
+            elif winning_move(board, 1):
+                return (None, -10000000000000)
+            else:  # Game is over, no more valid moves
+                return (None, 0)
+        else:  # Depth is zero
+            return (None, score_position(board, 2))
+
+    if maximizingPlayer:
+        value = -float('inf')
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 2)
+            new_score = minimax(temp_board, depth - 1, False)[1]
+            if new_score > value:
+                value = new_score
+                best_col = col
+
+        return best_col, value
+
+    else:  # Minimizing player
+        value = float('inf')
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 1)
+            new_score = minimax(temp_board, depth - 1, True)[1]
+            if new_score < value:
+                value = new_score
+                best_col = col
+
+        return best_col, value
+
+
+# implementation of the Alpha-Beta pruning algorithm
+def alphabeta(board, depth, alpha, beta, maximizingPlayer):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, 2):
+                return (None, 100000000000000)
+            elif winning_move(board, 1):
+                return (None, -10000000000000)
+            else:  # Game is over, no more valid moves
+                return (None, 0)
+        else:  # Depth is zero
+            return (None, score_position(board, 2))
+
+    if maximizingPlayer:
+        value = -float('inf')
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 2)
+            new_score = alphabeta(temp_board, depth - 1, alpha, beta, False)[1]
+            if new_score > value:
+                value = new_score
+                best_col = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return best_col, value
+
+    else:  # Minimizing player
+        value = float('inf')
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 1)
+            new_score = alphabeta(temp_board, depth - 1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                best_col = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return best_col, value
+
+
+# Get the valid move locations
+def get_valid_locations(board):
+    valid_locations = []
+    for col in range(COLUMN_COUNT):
+        if is_valid_location(board, col):
+            valid_locations.append(col)
+    return valid_locations
+
+
+# Check if the game is over
+def is_terminal_node(board):
+    return winning_move(board, 1) or winning_move(board, 2) or len(get_valid_locations(board)) == 0
+
+
+# Create the game board
+board = create_board()
+
+# Set up the Pygame font
+myfont = pygame.font.SysFont("monospace", 75)
+
+
+# Draw the game board on the GUI
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, RED, (c * SQUARE_SIZE, r * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.circle(screen, BLACK, (
+            int(c * SQUARE_SIZE + SQUARE_SIZE / 2), int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2)), RADIUS)
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, BLUE, (
+                int(c * SQUARE_SIZE + SQUARE_SIZE / 2), height - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, WHITE, (
+                int(c * SQUARE_SIZE + SQUARE_SIZE / 2), height - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)), RADIUS)
+    pygame.display.update()
+
+
+# Draw the game board on the GUI
+draw_board(board)
+game_over = False
+turn = 0
+
+
+def start_game_with_minimax(depth):
+    global turn, game_over
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if turn == 0:
+                # Computer player 1
+                col, minimax_score = minimax(board, depth, True)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+
+                    if winning_move(board, 1):
+                        label = myfont.render("BLUE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            else:
+
+                # Computer player 2
+                col = random.randint(0, COLUMN_COUNT - 1)
+                minimax_score = minimax(board, depth, False)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
+
+                    if winning_move(board, 2):
+                        label = myfont.render("WHITE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+                # Player move
+                posx = event.pos[0]
+                col = int(posx // SQUARE_SIZE)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+
+                    if winning_move(board, 1):
+                        label = myfont.render("BLUE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            if game_over:
+                pygame.time.wait(3000)
+                pygame.quit()
+                sys.exit()
+
+        # Update the display
+        pygame.display.update()
+
+
+def start_game_with_alpha(depth):
+    global turn, game_over
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if turn == 0:
+                # Computer player 1
+                col, minimax_score = alphabeta(board, depth, -float('inf'), float('inf'), True)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+
+                    if winning_move(board, 1):
+                        label = myfont.render("BLUE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            else:
+
+                # Computer player 2
+                col = random.randint(0, COLUMN_COUNT - 1)
+                minimax_score = alphabeta(board, depth, -float('inf'), float('inf'), False)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
+
+                    if winning_move(board, 2):
+                        label = myfont.render("WHITE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+                # Player move
+                posx = event.pos[0]
+                col = int(posx // SQUARE_SIZE)
+
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+
+                    if winning_move(board, 1):
+                        label = myfont.render("BLUE wins", 1, RED)
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        game_over = True
+
+                    turn += 1
+                    turn = turn % 2
+
+                    print_board(board)
+                    draw_board(board)
+
+            if game_over:
+                pygame.time.wait(3000)
+                pygame.quit()
+                sys.exit()
+
+        # Update the display
+        pygame.display.update()
+
+
+def get_depth(difficulty_level):
+    if difficulty_level == "Easy":
+        return 3
+    elif difficulty_level == "Medium":
+        return 4
+    elif difficulty_level == "Hard":
+        return 5
+
+
+window = tk.Tk()
+window.title("Connect 4")
+
+window.configure(bg="black")
+window.geometry("300x300")  # Set the width and height as desired
+
+algorithm_var = tk.StringVar(window)
+
+difficulty_label = tk.Label(window, text="Select Difficulty:", bg="black", fg="white", font=("Arial", 10, "italic"))
+difficulty_label.pack(pady=10)
+
+difficulty_var = tk.StringVar(window)
+difficulty_var.set("Level of Difficulty")
+difficulty_dropdown = tk.OptionMenu(window, difficulty_var, "Easy", "Medium", "Hard")
+difficulty_dropdown.configure(width=30, height=1, bg="Blue", fg="white")
+difficulty_dropdown["menu"].config(font=("Arial", 10, "italic"))
+difficulty_dropdown.pack()
+
+start_button = tk.Button(window, text="Start Alphabeta",
+                         command=lambda: start_game_with_alpha(get_depth(difficulty_var.get())))
+start_button.configure(width=15, height=2, bg="red", fg="white", font=("Arial", 10, "italic"))
+start_button.place(relx=0.48, rely=0.5, anchor=tk.E)
+
+start_button_MiniMax = tk.Button(window, text="Start Minimax",
+                                 command=lambda: start_game_with_minimax(get_depth(difficulty_var.get())))
+start_button_MiniMax.configure(width=15, height=2, bg="red", fg="white", font=("Arial", 10, "italic"))
+start_button_MiniMax.place(relx=0.52, rely=0.5, anchor=tk.W)
+
+window.mainloop()
